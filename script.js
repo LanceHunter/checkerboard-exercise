1ed3d7f08fff748ca74b86269a234f498bd488ac
Lance Hunter <lancehunter@gmail.com>
2017-11-03 10:55:51 -0500

    Completed gradient exercise.

--- script.js
+++ script.js
@@ -2,16 +2,23 @@
 
 (function() {
   let body = document.getElementsByTagName('body');
-  let color = " black";
+  let colorMarker = " black";
+  let gradient = 0;
+  let rowCount = 0;
   let container = document.createElement('div');
   for (i=1; i<64; i++) {
+    rowCount++;
+    if (rowCount===10) {
+      gradient += 31;
+      rowCount = 0;
+    }
     let square = document.createElement('div');
-      if (color===" red") {
-        square.style.backgroundColor = color;
-        color = " black";
+      if (colorMarker===" red") {
+        square.style.backgroundColor = ` rgb( ${255-gradient}, 0, 0)`;
+        colorMarker = " black";
       } else {
-        square.style.backgroundColor = color;
-        color = " red";
+        square.style.backgroundColor = ` rgb( ${gradient}, ${gradient}, ${gradient})`;
+        colorMarker = " red";
       }
       square.style.width = " 11.1%";
       square.style.paddingBottom = " 11.1%";
