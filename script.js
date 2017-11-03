befc2178419e06255358f20a1f8b2afc962893c4
Lance Hunter <lancehunter@gmail.com>
2017-11-03 10:15:02 -0500

    Completed initial checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Your JS goes here
+
+(function() {
+  let body = document.getElementsByTagName('body');
+  let blackSquare = document.createElement('div');
+  let color = " red";
+  let container = document.createElement('div');
+  for (i=1; i<64; i++) {
+    let square = document.createElement('div');
+      if (color===" red") {
+        square.style.backgroundColor = color;
+        color = " black";
+      } else {
+        square.style.backgroundColor = color;
+        color = " red";
+      }
+      square.style.width = " 11.1%";
+      square.style.paddingBottom = " 11.1%";
+      square.style.float = " left";
+      body[0].appendChild(square);
+  }
+})();
